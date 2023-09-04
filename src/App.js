4e7c7888da6395dd4f340509cd0ad8664a8158ef
webemnatt <104658987+webemnatt@users.noreply.github.com>
2023-09-04 17:28:22 -0300

    inserida imagem de background

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import React from 'react';
 import Alive from './Alive.js';
 import Background from './Background.js';
+import SpecialAds from './SpecialAds.js';
 import './style.scss';
 
 export default function App() {
   return (
-    <div class="container">
-      <Background />
-      <div class="content-head medium-centered medium-24 large-20 xlarge-12">
-        <div data-js="cb-page-root"></div>
+    <main
+      class="theme main-content"
+      itemscope=""
+      itemtype="http://schema.org/LiveBlogPosting"
+    >
+      <div class="cb-flex-wrapper">
+        <div class="container">
+          <Background />
+          <div class="content-head medium-centered medium-24 large-20 xlarge-12">
+            <div data-js="cb-page-root"></div>
 
-        <div class="content-ad">
-          <div class="propaganda-retangular">propaganda</div>
+            <div class="content-ad">
+              <div class="propaganda-retangular">propaganda</div>
+            </div>
+            <div class="sharebar">
+              <img
+                class="sharebar-icon"
+                src="share-icon.svg"
+                alt="ícone de compartilhamento"
+              />
+            </div>
+            <Alive />
+            <SpecialAds />
+            <div class="live-at">Transmitido ao vivo em 12/05/2022 06h27</div>
+            <h1
+              class="content-head__title"
+              data-acceptance="titulo"
+              data-js="header"
+            >
+              Cobertura ao vivo do lançamento da nova coleção de primavera-verão
+              da grife "Will be Mine"
+            </h1>
+          </div>
         </div>
-        <div class="sharebar">
-          <img
-            class="sharebar-icon"
-            src="share-icon.svg"
-            alt="ícone de compartilhamento"
-          />
-        </div>
-        <Alive />
-        <div class="live-at">Transmitido ao vivo em 12/05/2022 06h27</div>
-        <h1
-          class="content-head__title"
-          data-acceptance="titulo"
-          data-js="header"
-        >
-          Cobertura ao vivo do lançamento da nova coleção de primavera-verão da
-          grife "Will be Mine"
-        </h1>
       </div>
-    </div>
+    </main>
   );
 }
---
+++ src/SpecialAds.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+export default function SpecialAds() {
+  return (
+    <div class="row content-branded-content">
+      <div class="column medium-centered medium-19 large-18 branded-content-signature__top">
+        <span class="branded-content-label">Especial Publicitário</span>
+      </div>
+    </div>
+  );
+}
